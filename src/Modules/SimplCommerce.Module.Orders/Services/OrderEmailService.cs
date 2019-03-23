d67ae1bce5447340f66cde67bf10a4671c35dcd1
IamGanesh19 <IamGanesh19@users.noreply.github.com>
2019-03-23 23:43:22 +0530

    Theme support for Order Email. Sample order message for Asrita Silks

--- src/Modules/SimplCommerce.Module.Orders/Services/OrderEmailService.cs
+++ src/Modules/SimplCommerce.Module.Orders/Services/OrderEmailService.cs
@@ -1,4 +1,7 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Configuration;
 using SimplCommerce.Infrastructure.Web;
 using SimplCommerce.Module.Core.Models;
 using SimplCommerce.Module.Core.Services;
@@ -10,16 +13,31 @@ namespace SimplCommerce.Module.Orders.Services
     {
         private readonly IEmailSender _emailSender;
         private readonly IRazorViewRenderer _viewRender;
-        public OrderEmailService(IEmailSender emailSender, IRazorViewRenderer viewRender)
+        private readonly IRazorViewEngine _viewEngine;
+        private string theme;
+
+        public OrderEmailService(IEmailSender emailSender, IRazorViewRenderer viewRender, IConfiguration config, IRazorViewEngine viewEngine)
         {
             _emailSender = emailSender;
             _viewRender = viewRender;
+            _viewEngine = viewEngine;
+            theme = config["Theme"];
         }
 
         public async Task SendEmailToUser(User user, Order order)
         {
-            var emailBody = await _viewRender.RenderViewToStringAsync("/Areas/Orders/Views/EmailTemplates/OrderEmailToCustomer.cshtml", order);
-            var emailSubject = $"Order information #{order.Id}";
+            var viewPath = "/Areas/Orders/Views/EmailTemplates/OrderEmailToCustomer.cshtml";
+            if (!string.IsNullOrWhiteSpace(theme) && !string.Equals(theme, "Generic", System.StringComparison.InvariantCultureIgnoreCase))
+            {
+                var themeViewPath = $"/Themes/{theme}{viewPath}";
+                var result = _viewEngine.GetView("", themeViewPath, isMainPage: false);
+                if (result.Success)
+                {
+                    viewPath = themeViewPath;
+                }
+            }
+            var emailBody = await _viewRender.RenderViewToStringAsync(viewPath, order);
+            var emailSubject = $"Order Confirmation";
             await _emailSender.SendEmailAsync(user.Email, emailSubject, emailBody, true);
         }
     }
