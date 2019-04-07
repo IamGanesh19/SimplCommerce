6aadfb928646a75e9205a5a01dd8612e07a1e02b
IamGanesh19 <IamGanesh19@users.noreply.github.com>
2019-04-07 11:49:46 +0530

    Shipment Tracking background job initial commit

--- src/Modules/SimplCommerce.Module.Shipments/Services/ShipmentStatusUpdateBackgroundService.cs
+++ src/Modules/SimplCommerce.Module.Shipments/Services/ShipmentStatusUpdateBackgroundService.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SimplCommerce.Infrastructure.Data;
+using SimplCommerce.Module.Orders.Models;
 using SimplCommerce.Module.Shipments.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,22 +27,46 @@ namespace SimplCommerce.Module.Shipments.Services
 
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            //_logger.LogInformation("ShipmentStatusUpdateBackgroundService is starting.");
-            //while (!stoppingToken.IsCancellationRequested)
-            //{
-            //    _logger.LogInformation("ShipmentStatusUpdateBackgroundService is working.");
-            //    using (var scope = _serviceProvider.CreateScope())
-            //    {
-            //        var orderRepository = scope.ServiceProvider.GetRequiredService<IRepository<Shipment>>();
-            //        //var orderService = scope.ServiceProvider.GetRequiredService<IOrderService>();
-            //        //var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-            //        //await CancelFailedPaymentOrders(orderRepository, orderService, mediator, stoppingToken);
-            //    }
+            _logger.LogInformation("ShipmentStatusUpdateBackgroundService is starting.");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("ShipmentStatusUpdateBackgroundService is working.");
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var shipmentRepository = scope.ServiceProvider.GetRequiredService<IRepository<Shipment>>();
+                    var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                    await UpdateOrderStatus(shipmentRepository, mediator, stoppingToken);
+                }
 
-            //    await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+                await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+            }
+
+            _logger.LogInformation("ShipmentStatusUpdateBackgroundService is stopping.");
+        }
+
+        private async Task UpdateOrderStatus(IRepository<Shipment> shipmentRepository, IMediator mediator, CancellationToken stoppingToken)
+        {
+            var shipmentsToTrack = shipmentRepository.Query().Where(x =>
+                x.Order.OrderStatus == OrderStatus.Shipped
+                && !string.IsNullOrWhiteSpace(x.TrackingNumber));
+
+            //foreach (var order in shipmentsToTrack)
+            //{
+            //    orderService.CancelOrder(order);
+            //    var orderStatusChanged = new OrderChanged
+            //    {
+            //        OrderId = order.Id,
+            //        OldStatus = OrderStatus.PendingPayment,
+            //        NewStatus = OrderStatus.Canceled,
+            //        UserId = SystemUserId,
+            //        Order = order,
+            //        Note = "System canceled"
+            //    };
+
+            //    await mediator.Publish(orderStatusChanged, stoppingToken);
             //}
 
-            //_logger.LogInformation("ShipmentStatusUpdateBackgroundService is stopping.");
+            //await orderRepository.SaveChangesAsync();
         }
     }
 }
