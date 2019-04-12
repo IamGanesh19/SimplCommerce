094c5df80bde26ae936a2d6f3ec4c69c5d1dbe49
IamGanesh19 <IamGanesh19@users.noreply.github.com>
2019-04-13 00:41:56 +0530

    User enabled editing mobile number. Added DB migration for Aftership tracking

---
+++ src/Modules/SimplCommerce.Module.ShipmentTrackingAfterShip/Data/ShipmentTrackerAfterShipCustomModelBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplCommerce.Infrastructure.Data;
+using SimplCommerce.Module.Core.Models;
+
+namespace SimplCommerce.Module.ShipmentTrackingAfterShip.Data
+{
+    public class ShipmentTrackerAfterShipCustomModelBuilder : ICustomModelBuilder
+    {
+        public void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppSetting>().HasData(
+                new AppSetting("Aftership.ApiKey") { Module = "ShipmentTrackingAfterShip", IsVisibleInCommonSettingPage = false, Value = "" }
+            );
+        }
+    }
+}
